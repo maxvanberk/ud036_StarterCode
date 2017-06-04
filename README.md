efd049d030a6b5574331e8cd1b05f77a645b8870
maxvanberk <vanberk@outlook.com>
2017-06-04 12:16:48 +0200

    changed source
    changed source

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# ud036_StarterCode
+# Movie Site
 Source code for a Movie Trailer website.
+
+
+How to change the movies on your site:
+
+1) Open the file movie_site.py with a text editor and change the 
+	instances of the class Movies to your favorite movies. Arguments are title, storyline, poster image and trailer.
+2) Add those movies to the list "movies"
+
+How to run this site:
+
+1) Run the move_site.py within your Python intepreter. A browser window will open up and show your movie site
+
--- movie_site.py
+++ movie_site.py
@@ -1,30 +1,45 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-                       "A story of a boy and his toys that come to live",
-                       "https://images-na.ssl-images-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_SY1000_SX670_AL_.jpg",
-                       "https://youtu.be/KYz2wyBy3kc")
+the_shining = media.Movie("The Shining",
+                          "Man get's a bad case of "
+                            "cabin fever but finally chills",
+                            "http://t3.gstatic.com/images?q=tbn:ANd9GcSO0FTIrcYQ_r6QDHclHKOKBHMY9FlUENGVRUYpcHCEGRUaHupG",  # noqa
+                            "https://www.youtube.com/watch?v=5Cb3ik6zP2I")
 
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "https://goo.gl/images/0pyHPM",
-                     "https://youtu.be/5PSNL1qE6VY")
+the_matrix = media.Movie("The Matrix",
+                         "Batteries go bad",
+                            "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",  # noqa
+                            "https://www.youtube.com/watch?v=m8e-FF8MsqU"
+                         )
+
+full_metal_jacket = media.Movie("Full Metal Jacket",
+                                "Hilarious drill sergeant trolls a dip-shit"
+                                " ...then it turns out that war is unpleasant",
+                            "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg",  # noqa
+                                "https://youtu.be/x9f6JaaX7Wg")
+
+twelve_monkeys = media.Movie("Twelve Monkeys",
+                             "People from the future repeatedly bend Bruce Willis "
+                             "back in time to save the world. He fails",
+                            "https://upload.wikimedia.org/wikipedia/en/c/cf/Twelve_monkeysmp.jpg",  # noqa
+                             "https://youtu.be/15s4Y9ffW_o")
+
+alien = media.Movie("Alien",
+                    "Crew of commercial deep space vessel learns the hard "
+                            "way why medical safety protocols exist",
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BNDNhN2IxZWItNGEwYS00ZDNhLThiM2UtODU3NWJlZjBkYjQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,681,1000_AL_.jpg",  # noqa
+                            "https://www.youtube.com/watch?v=LjLamj-b0I8")
 
 
 fight_club = media.Movie("Fight Club",
-                        "Man's chronic insomnia gets out of hand",
-                         "https://goo.gl/images/YtrL8W",
+                         "Man's chronic insomnia gets out of hand",
+                         "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg",
                          "https://youtu.be/SUXWAEX2jlg")
 
-print(media.Movie.__doc__)
-print(media.Movie.__name__)
-print(media.Movie.__module__)
-      
-movies=[toy_story,avatar,fight_club]
+movies = [the_shining, the_matrix, full_metal_jacket,
+          twelve_monkeys, alien, fight_club]
 
 fresh_tomatoes.open_movies_page(movies)
 
-                        
-
-                       
+print(full_metal_jacket.storyline)
